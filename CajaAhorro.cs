b6c2302e0a20518c2cbf791029e3f54e4a3734f6
fede <federicoalcaraz24@gmail.com>
2026-05-03 07:17:58 -0300

    Agregué la caja de ahorros al p*to main

---
+++ CajaAhorro.cs
@@ -0,0 +1,31 @@
+public class CajaAhorro : Cuenta {
+    public CajaAhorro(string titular, int cbu) : base(titular, cbu){
+        TipoCuenta = TipoCuenta.CajaDeAhorro;
+        MargenDeuda = 0;
+    }
+    public override void Transferir(Cuenta cuentaDestino, decimal monto){
+        if(ValidarTransferencia(monto)){
+            Retirar(monto);
+            cuentaDestino.Depositar(monto);
+        }
+    }
+    public override bool ValidarTransferencia(decimal monto){
+        return monto > 0 && Saldo >= monto;
+    }
+    public override void Retirar(decimal monto){
+        if(ValidarRetiro(monto)){
+            Saldo -= monto;
+        }
+    }
+    public override bool ValidarRetiro(decimal monto){
+        return monto > 0 && Saldo >= monto;
+    }
+    public override void Depositar(decimal monto){
+        if(ValidarDeposito(monto)){
+            Saldo += monto;
+        }
+    }
+    public override bool ValidarDeposito(decimal monto){
+        return monto > 0;
+    }
+}
