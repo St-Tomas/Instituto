4579e8d97f055f3526f53f7801fbc39902831da3
St-Tomas <tomastrevensky@gmail.com>
2026-05-04 22:53:51 -0300

    Correcion de cuenta

--- Cuenta.cs
+++ Cuenta.cs
@@ -3,12 +3,12 @@ using System.Numerics;
 
 public abstract class Cuenta
 {
-    private string Titular {get; init;}
-    private decimal Saldo {get; set;}
-    private int CBU {get; init;}
-    private TipoCuenta TipoCuenta;
-    private string NumeroCuenta;
-    private decimal MargenDeuda;
+    public string Titular {get; init;}
+    public decimal Saldo {get; set;}
+    public int CBU {get; init;}
+    public TipoCuenta TipoCuenta;
+    public string NumeroCuenta;
+    public decimal MargenDeuda;
 
     public Cuenta (string titular, int cbu)
     {
@@ -16,9 +16,9 @@ public abstract class Cuenta
         Saldo = 0;
         CBU = cbu;
     }
-    public abstract void Depositar ();
-    public abstract void Transferir(Cuenta cuentaInicio, Cuenta cuentaDestino);
-    public abstract Boolean ValidarDeposito();
-    public abstract Boolean ValidarRetiro();
-    public abstract Boolean ValidarTransferencia();
+    public abstract void Depositar (decimal monto);
+    public abstract void Transferir(Cuenta cuentaDestino, decimal monto);
+    public abstract Boolean ValidarDeposito(decimal monto);
+    public abstract Boolean ValidarRetiro(decimal monto);
+    public abstract Boolean ValidarTransferencia(decimal monto);
 }
